e25521c2a1bd073f261c074475d574070263259b
Alizbar\andr9 <st128876@student.spbu.ru>
2024-09-29 02:36:46 +0300

    ttt

--- Task3/t3.cpp
+++ Task3/t3.cpp
@@ -6,19 +6,19 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 
-    int x1 = 0;
-    int m1 = 0;
-    int r1 = 0;
-    int x2 = 0;
-    int m2 = 0;
-    int r2 = 0;
+    float x1 = 0;
+    float m1 = 0;
+    float r1 = 0;
+    float x2 = 0;
+    float m2 = 0;
+    float r2 = 0;
 
-    scanf_s("%d %d %d", &x1, &m1, &r1);
-    scanf_s("%d %d %d", &x2, &m2, &r2);
+    scanf_s("%f %f %f", &x1, &m1, &r1);
+    scanf_s("%f %f %f", &x2, &m2, &r2);
 
-    double rast = sqrt(((x2 - x1) * (x2 - x1)) + ((m2 - m1) * (m2 - m1)));
+    float rast = sqrt(((x2 - x1) * (x2 - x1)) + ((m2 - m1) * (m2 - m1)));
 
-    if ((rast < (r1 + r2)) or (rast == (r1 + r2)) and ((rast + r1) > r2 or (rast + r1) == r2) and ((rast + r2) > r1 or (rast + r2) == r1)) {
+    if (((rast < (r1 + r2)) or (rast == (r1 + r2))) and ((rast + r1) > r2 or (rast + r1) == r2) and ((rast + r2) > r1 or (rast + r2) == r1)) {
         printf("YES");
     }
     else {
