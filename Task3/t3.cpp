5bb9b97e87d91467638f720b34714eb1ffd23693
Alizbar\andr9 <st128876@student.spbu.ru>
2024-09-28 23:07:31 +0300

    a

--- Task3/t3.cpp
+++ Task3/t3.cpp
@@ -6,7 +6,6 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 
-
     int x1 = 0;
     int y1 = 0;
     int r1 = 0;
@@ -17,9 +16,9 @@ int main(int argc, char* argv[])
     scanf_s("%d %d %d", &x1, &y1, &r1);
     scanf_s("%d %d %d", &x2, &y2, &r2);
 
-    int rast = pow(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)), 0.5);
+    float rast = sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
 
-    if ((rast < (r1 + r2)) or (rast == (r1 + r2)) and (((rast + r1) > r2 or (rast + r1) == r2)) and (((rast + r2) > r1 or (rast + r2) == r1))) {
+    if ((rast < (r1 + r2)) or (rast == (r1 + r2)) and ((rast + r1) > r2 or (rast + r1) == r2) and ((rast + r2) > r1 or (rast + r2) == r1)) {
         printf("YES");
     }
     else {
